cf00800436251de48b0fdc9c3818d10394a01799
Aashish70 <ashishsingh70002@gmail.com>
2024-10-30 19:45:12 +0530

    add models for Comment, Like, Playlist, Subscription, Tweet, User, and Video with Mongoose schemas

---
+++ src/models/user.model.js
@@ -0,0 +1,61 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+    {
+        username: {
+            type: String,
+            required: true,
+            unique: true,
+            trim: true,
+            lowercase: true,
+            index: true,
+        },
+
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            lowercase: true,
+            trim: true,
+        },
+
+        fullName: {
+            type: String,
+            required: true,
+            trim: true,
+            index: true,
+        },
+
+        avatar: {
+            type: String, // cloudnary url
+            required: true,
+        },
+
+        coverImage: {
+            type: String, // cloudnary url
+        },
+
+        watchHistory: [
+            {
+              type: Schema.Types.ObjectId,
+              ref: "Video",
+            },
+        ],
+
+        password: {
+            type: String,
+            required: [true, "Password is required"],
+        },
+
+        refreshToken: {
+            type: String,
+        },
+    },
+    
+    {
+        timestamps: true,
+    }
+
+);
+
+export const User = mongoose.model("User", userSchema);
